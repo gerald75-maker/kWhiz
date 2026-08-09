912ab40826f9d26985c12cad34ce4c05c8cd4cdd
Gérald Aubard <gerald.aubard@gmail.com>
2026-08-09 15:42:31 +0200

    Publie les correctifs de fiabilité en version 2.24.2

--- tests/version-consistency.test.js
+++ tests/version-consistency.test.js
@@ -15,5 +15,5 @@ test('toutes les versions visibles utilisent la variable de build', () => {
 });
 
 test('la version du package est celle de la livraison', () => {
-    assert.equal(packageJson.version, '2.24.1');
+    assert.equal(packageJson.version, '2.24.2');
 });
